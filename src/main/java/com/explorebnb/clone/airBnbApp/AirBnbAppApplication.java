8b900088749dcdad4df34a854fe8abdc8752997e
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2024-12-22 18:55:45 +0530

    Added Entities

---
+++ src/main/java/com/explorebnb/clone/airBnbApp/AirBnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.explorebnb.clone.airBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+	}
+
+}
